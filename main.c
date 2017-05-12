55341ba26db21790553fdf532fcfec14a5c1d12b
Jan Hendrik Farr <janhendrik.farr@gmail.com>
2017-05-12 21:49:17 +0200

    removed x position

--- main.c
+++ main.c
@@ -6,8 +6,6 @@ typedef struct element {
 	double x;
 	double y;
 
-	//bottom left corner
-	double x_pos;
 	double y_pos;
 
 	int constant;
@@ -30,8 +28,6 @@ double elementIx(const element* e) {
 }
 
 typedef struct stringer {
-	//bottom left corner
-	double x_pos;
 	double y_pos;
 	
 	element e1;
@@ -42,11 +38,9 @@ typedef struct stringer {
 	struct stringer* next;
 } stringer;
 
-stringer* newStringer(double x, double y, double w, double t, stringer* next) {
-	stringer s = {x, y, {w, t, x, y, 1}, {t, w - t, x, y + t, 1}, 1, next};
-	stringer* ps = (stringer*) malloc(sizeof(stringer));
-	*ps = s;
-	return ps;
+stringer newStringer(double y, double w, double t) {
+	stringer s = {y, {w, t, y, 1}, {t, w - t, y + t, 1}, 1};
+	return s;
 }
 
 double stringerArea(const stringer* s) {
@@ -65,17 +59,18 @@ double stringerIx(const stringer* s) {
 	return elementIx(&(s->e1)) + elementArea(&(s->e1)) * (elementY(&(s->e1)) - stringerY(s)) * (elementY(&(s->e1)) - stringerY(s)) + elementIx(&(s->e2)) + elementArea(&(s->e2)) * (elementY(&(s->e2)) - stringerY(s)) * (elementY(&(s->e2)) - stringerY(s)) + stringerArea(s) * stringerY(s) * stringerY(s);
 }
 
-int main() {
-	element e = {0.1f, 0.1f, 0.0f, 0.0f, .constant = 1};
 
-	stringer* s = newStringer(0.0f, 10.0f, 20.0f, 1.5f, NULL);
+
+int main() {
+	element e = {0.1f, 0.1f, 0.0f, .constant = 1};
+
+	stringer s = newStringer(10.0f, 20.0f, 1.5f);
 	
-	printf("%f\n", stringerIx(s));
-	printf("%f\n", stringerY(s));
+	printf("%f\n", stringerIx(&s));
+	printf("%f\n", stringerY(&s));
 	//printf("%f\n", elementArea(&e));
 	//printf("%f\n", elementY(&e));
 	//printf("%f\n", elementQ(&e));
 	//printf("%f\n", elementIx(&e));
-	free(s);
 	return 0;
 }
