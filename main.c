abed237f8a5feb40fb8b57acbde382fa4e3c4a6d
Jan Hendrik Farr <janhendrik.farr@gmail.com>
2017-05-13 23:20:42 +0000

    added material and panel struct + cleanup

--- main.c
+++ main.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+#define PANEL_WIDTH 400
+#define PANEL_LENGTH 500
+
+
+typedef struct material{
+	double E;
+	double rho;
+	double sigmaYield;
+	double sigmaUlt;
+} material;
+
 typedef struct element {
 	double x;
 	double y;
@@ -34,8 +46,6 @@ typedef struct stringer {
 	element e2;
 
 	int constant;
-
-	struct stringer* next;
 } stringer;
 
 stringer newStringer(double y, double w, double t) {
@@ -44,11 +54,11 @@ stringer newStringer(double y, double w, double t) {
 }
 
 double stringerArea(const stringer* s) {
-	return elementArea(&(s->e1)) + elementArea(&(s->e2));
+	return s->constant * (elementArea(&(s->e1)) + elementArea(&(s->e2)));
 }
 
 double stringerQ(const stringer* s) {
-	return elementQ(&(s->e1)) + elementQ(&(s->e2));
+	return s->constant * (elementQ(&(s->e1)) + elementQ(&(s->e2)));
 }
 
 double stringerY(const stringer* s) {
@@ -56,18 +66,51 @@ double stringerY(const stringer* s) {
 }
 
 double stringerIx(const stringer* s) {
-	return elementIx(&(s->e1)) + elementArea(&(s->e1)) * (elementY(&(s->e1)) - stringerY(s)) * (elementY(&(s->e1)) - stringerY(s)) + elementIx(&(s->e2)) + elementArea(&(s->e2)) * (elementY(&(s->e2)) - stringerY(s)) * (elementY(&(s->e2)) - stringerY(s)) + stringerArea(s) * stringerY(s) * stringerY(s);
+	return s->constant * (elementIx(&(s->e1)) + elementArea(&(s->e1)) * (elementY(&(s->e1)) - stringerY(s)) * (elementY(&(s->e1)) - stringerY(s)) + elementIx(&(s->e2)) + elementArea(&(s->e2)) * (elementY(&(s->e2)) - stringerY(s)) * (elementY(&(s->e2)) - stringerY(s))/* + stringerArea(s) * stringerY(s) * stringerY(s)*/);
 }
 
+typedef struct panel {
+	element sheet;
+	stringer stringer;
+	int numberOfStringers;
+} panel;
 
+panel newPanel(int n, double sheetThickness, double stringerWidth, double stringerThickness, material m) {
+	stringer stringer = newStringer(sheetThickness, stringerWidth, stringerThickness);
+	element sheet = {PANEL_WIDTH, sheetThickness, 0, 1};
+	panel p = {sheet, stringer, n};
+
+	return p;
+}
+
+double panelArea(const panel* p) {
+	return elementArea(&(p->sheet)) + p->numberOfStringers * stringerArea(&(p->stringer));
+}
+
+double panelQ(const panel* p) {
+	return elementQ(&(p->sheet)) + p->numberOfStringers * stringerQ(&(p->stringer));
+}
+
+double panelY(const panel* p) {
+	return panelQ(p) / panelArea(p);
+}
+
+double panelIx(const panel* p) {
+	return elementIx(&(p->sheet)) + elementArea(&(p->sheet)) * (elementY(&(p->sheet)) - panelY(p)) * (elementY(&(p->sheet)) - panelY(p)) + p->numberOfStringers * (stringerIx(&(p->stringer)) + stringerArea(&(p->stringer)) * (stringerY(&(p->stringer)) - panelY(p)) * (stringerY(&(p->stringer)) - panelY(p)));
+}
 
 int main() {
 	element e = {0.1f, 0.1f, 0.0f, .constant = 1};
 
-	stringer s = newStringer(10.0f, 20.0f, 1.5f);
-	
-	printf("%f\n", stringerIx(&s));
+	stringer s = newStringer(0.0f, 20.0f, 1.5f);
+	material al = {0,0,0,0};
+	panel p = newPanel(3, 0.8f, 20.0f, 1.5f, al);
+
+	printf("%f\n", panelY(&p));
+	printf("%f\n", stringerIx(&(p.stringer)));
+	printf("%f\n", stringerY(&(p.stringer)));
 	printf("%f\n", stringerY(&s));
+	printf("%f\n", panelIx(&p));
 	//printf("%f\n", elementArea(&e));
 	//printf("%f\n", elementY(&e));
 	//printf("%f\n", elementQ(&e));
